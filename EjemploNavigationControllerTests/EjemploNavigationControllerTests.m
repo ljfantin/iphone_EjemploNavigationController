41c375b322d5b3791bc9f86fc3d198e78a30356c
Leandro Fantin <lfantin@mac-lfantin.local>
2014-07-14 16:37:01 -0300

    Initial Commit

---
+++ EjemploNavigationControllerTests/EjemploNavigationControllerTests.m
@@ -0,0 +1,34 @@
+//
+//  EjemploNavigationControllerTests.m
+//  EjemploNavigationControllerTests
+//
+//  Created by Leandro Fantin on 14/07/14.
+//  Copyright (c) 2014 mercadolibre. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EjemploNavigationControllerTests : XCTestCase
+
+@end
+
+@implementation EjemploNavigationControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
